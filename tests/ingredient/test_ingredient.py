975e8e722992d3c07678e405d397ac76270e79ca
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-20 18:57:25 -0300

    feat: req 1 done

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    recipe_1 = Ingredient('farinha')
+    assert recipe_1.name == 'farinha'
+
+    assert recipe_1.restrictions == {
+        Restriction.GLUTEN,
+    }
+
+    assert hash('farinha') != hash(Ingredient('tomate'))
+    assert hash('farinha') == hash(Ingredient('farinha'))
+    assert recipe_1 == Ingredient('farinha')
+    assert recipe_1 != Ingredient('tomate')
+
+    assert repr(recipe_1) == "Ingredient('farinha')"
